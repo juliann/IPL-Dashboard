c6c3c39ff3c4d74ba489d31dbcdf27351d231df4
Julian <julian.nadarz@gmail.com>
2021-06-01 21:45:38 +0200

    added react

---
+++ src/frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {TeamPage} from "./pages/TeamPage";
+
+function App() {
+    return (
+        <div className="App">
+           <h1>Hi</h1>
+        </div>
+    );
+}
+
+export default App;
